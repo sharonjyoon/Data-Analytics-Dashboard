a40c22528ab78bc026128c91dd582fd6cdc7aa68
Sharon Yoon <sharonjyoon@gmail.com>
2022-08-23 00:45:01 -0700

    finished calendar app and kanban app

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { HtmlEditor, Image, Inject, Link, QuickLink, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
+
+import { EditorData } from '../data/data/dummy';
+import { Header } from '../components';
 
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div>
+      
+    </div>
   )
 }
 
