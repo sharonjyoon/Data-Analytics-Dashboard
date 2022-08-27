b21ca2ea2af586aeec0c789607001dd82d588137
Sharon Yoon <sharonjyoon@gmail.com>
2022-08-27 02:01:12 -0700

    stacked chart complete

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
 
 import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
 
 const Stacked = ({width, height}) => {
+  const { currentMode } = useStateContext();
   return (
     <ChartComponent
     width={width}
